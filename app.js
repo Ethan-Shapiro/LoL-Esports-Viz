196e20d44492aa0000b07039b77d94075933c200
Ethan-Shapiro <46407744+Ethan-Shapiro@users.noreply.github.com>
2023-06-05 22:00:30 -0700

    finished all plots

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 function loadPlots() {
   part1();
-  // part2();
+  part2();
   part3();
+  part4();
 }
 
 function part1() {
@@ -92,7 +93,6 @@ function part1() {
     // Set the initial selection
     radioButtons.property("checked", (d, i) => i === 0);
 
-    // Function to update the chart based on the selected league
     function updateChart() {
       const selectedLeague = d3
         .select("input[name='league']:checked")
@@ -103,9 +103,8 @@ function part1() {
       xScale.domain(d3.extent(filteredData, (d) => d.Year));
       yScale.domain([0, 1]);
 
-      // Remove existing lines and points
-      svg.selectAll(".line").remove();
-      svg.selectAll("#point").remove();
+      // Remove existing x-axis
+      svg.select(".x-axis").remove();
 
       // Add the x-axis
       svg
@@ -114,14 +113,21 @@ function part1() {
         .attr("transform", `translate(0, ${height})`)
         .call(xAxis);
 
-      // Add the y-axis
-      svg.append("g").attr("class", "y-axis").call(yAxis);
+      // Remove existing lines and points
+      svg.selectAll(".line").remove();
+      svg.selectAll("#point").remove();
 
       var line = d3
         .line()
         .x((d) => xScale(d.Year))
         .y((d) => yScale(d.WinPercentage))
         .curve(d3.curveMonotoneX);
+
+      // Remove existing y-axis
+      svg.select(".y-axis").remove();
+
+      // Add the y-axis
+      svg.append("g").attr("class", "y-axis").call(yAxis);
 
       // Add lines and points for each team
       const teams = Array.from(new Set(filteredData.map((d) => d.TeamName)));
@@ -169,6 +175,7 @@ function part1() {
           .on("mouseover", handleMouseOver) // Attach mouseover event handler
           .on("mouseout", handleMouseOut); // Attach mouseout event handler;
       });
+
       // Event handler for mouseover event
       function handleMouseOver(event, d) {
         // Set tooltip content
@@ -340,7 +347,7 @@ function part2() {
     }
 
     // Event listener for force toggle
-    d3.select("body")
+    d3.select("#part2")
       .append("div")
       .attr("class", "force-toggle")
       .html(
@@ -403,8 +410,8 @@ function part2() {
 
 function part3() {
   const margin = { top: 20, right: 30, bottom: 30, left: 60 };
-  const width = 900 - margin.left - margin.right;
-  const height = 700 - margin.top - margin.bottom;
+  const width = 1200 - margin.left - margin.right;
+  const height = 1000 - margin.top - margin.bottom;
   var mapContainer = d3
     .select("#plot-3-container")
     .append("svg")
@@ -412,18 +419,372 @@ function part3() {
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
     .attr("transform", `translate(${margin.left},${margin.top})`);
-  d3.json("new.geojson").then(function (geoData) {
-    console.log(geoData);
-    // Code for visualization goes here]
-    var projection = d3.geoMercator().fitSize([width, height], geoData);
-    var path = d3.geoPath().projection(projection);
 
+  const legendContainer = mapContainer
+    .append("g")
+    .attr("class", "legend-container")
+    .attr("transform", `translate(${width - 120}, ${height - 150})`);
+  //TO DO: Create projection and pathgeo variables for world
+  const projection = d3
+    .geoNaturalEarth1()
+    .center([0, 0])
+    .scale(225)
+    .translate([width / 2, height / 2]);
+
+  const pathgeo = d3.geoPath().projection(projection);
+
+  function classMapper(countryName) {
+    const countries = [
+      "France",
+      "Ireland",
+      "United Kingdom",
+      "Germany",
+      "Norway",
+      "Sweden",
+      "Finland",
+      "Denmark",
+      "Netherlands",
+      "Mexico",
+      "Honduras",
+      "Guatemala",
+      "El Salvador",
+      "Nicaragua",
+      "Columbia",
+      "Peru",
+      "Bolivia",
+      "Chile",
+      "Paraguay",
+      "Argentina",
+      "USA",
+      "Canada",
+      "South Korea",
+      "China",
+    ];
+    if (countries.includes(countryName)) return "tochange";
+    return "worldpath";
+  }
+
+  function idMapper(id) {
+    const europeCountries = [
+      "France",
+      "Ireland",
+      "United Kingdom",
+      "Germany",
+      "Norway",
+      "Sweden",
+      "Finland",
+      "Denmark",
+      "Netherlands",
+    ];
+    const latinCountries = [
+      "Mexico",
+      "Honduras",
+      "Guatemala",
+      "El Salvador",
+      "Nicaragua",
+      "Columbia",
+      "Peru",
+      "Bolivia",
+      "Chile",
+      "Paraguay",
+      "Argentina",
+    ];
+    const northCountries = ["USA", "Canada"];
+
+    if (europeCountries.includes(id)) {
+      return "europe";
+    } else if (latinCountries.includes(id)) {
+      return "latin";
+    } else if (northCountries.includes(id)) {
+      return "america";
+    }
+    return id;
+  }
+  const legendWidth = 20;
+  const legendHeight = 150;
+  //TO DO: Load JSON file and create the map
+  d3.json("world.json").then((map) => {
     mapContainer
-      .selectAll("path")
-      .data(geoData.features)
+      .selectAll(".worldpath")
+      .data(map.features)
       .enter()
       .append("path")
-      .attr("d", path)
-      .attr("stroke", "white");
+      .attr("class", (d) => classMapper(d.properties.name))
+      .attr("id", (d) => idMapper(d.properties.name))
+      .attr("d", pathgeo)
+      .attr("fill", "grey")
+      .style("opacity", 0.25)
+      .style("stroke-width", 2)
+      .style("stroke", "black")
+      .style("stroke-opacity", 0.5);
+
+    const regionsMap = {
+      america: "LCS",
+      "South Korea": "LCK",
+      China: "LPL",
+      latin: "LLA",
+      europe: "LEC",
+    };
+
+    // Load the CSV data
+    d3.csv("regional_data.csv").then((data) => {
+      // Convert numerical values to numbers
+      data.forEach((d) => {
+        d.year = +d.year;
+        d.deaths = +d.deaths;
+        d.assists = +d.assists;
+        d.kills = +d.kills;
+        d.damagetochampions = +d.damagetochampions;
+        d.wardsplaced = +d.wardsplaced;
+        d.wardskilled = +d.wardskilled;
+        d.controlwardsbought = +d.controlwardsbought;
+        d.totalgold = +d.totalgold;
+        d.totalcs = +d.totalcs;
+      });
+
+      // Create a slider for years
+      const years = data.map((d) => d.year);
+      const sliderContainer = d3.select("#slider-container");
+
+      const slider = d3
+        .sliderBottom()
+        .min(d3.min(years))
+        .max(d3.max(years))
+        .width(800)
+        .tickFormat(d3.format("d"))
+        .ticks(5)
+        .step(1)
+        .default(d3.max(years))
+        .on("onchange", (year) => {
+          // Update the map based on the selected year
+          updateMap(year);
+        });
+
+      sliderContainer
+        .append("svg")
+        .attr("width", 900)
+        .attr("height", 100)
+        .append("g")
+        .attr("transform", "translate(30,30)")
+        .call(slider);
+
+      // Create a dropdown for statistics
+      const statsContainer = d3.select("#stats-container");
+
+      const stats = [
+        { value: "deaths", label: "Deaths" },
+        { value: "assists", label: "Assists" },
+        { value: "kills", label: "Kills" },
+        { value: "damagetochampions", label: "Damage to Champions" },
+        { value: "wardsplaced", label: "Wards Placed" },
+        { value: "wardskilled", label: "Wards Killed" },
+        { value: "controlwardsbought", label: "Control Wards Bought" },
+        { value: "totalgold", label: "Total Gold" },
+        { value: "total cs", label: "Total CS" },
+      ];
+
+      const statDropdown = statsContainer
+        .append("select")
+        .attr("id", "stat-dropdown")
+        .on("change", () => {
+          // Update the map based on the selected statistic
+          const selectedStat = document.getElementById("stat-dropdown").value;
+          updateMap(slider.value(), selectedStat);
+        });
+
+      statDropdown
+        .selectAll("option")
+        .data(stats)
+        .enter()
+        .append("option")
+        .attr("value", (d) => d.value)
+        .text((d) => d.label);
+
+      function updateLegend(colorScale) {
+        // Remove existing legend
+        legendContainer.selectAll("*").remove();
+
+        const legendAxis = d3
+          .axisRight()
+          .scale(
+            d3
+              .scaleLinear()
+              .range([legendHeight, 0])
+              .domain(colorScale.domain())
+          )
+          .ticks(5);
+
+        legendContainer
+          .append("g")
+          .attr("class", "legend-axis")
+          .attr("transform", `translate(${legendWidth}, 0)`)
+          .call(legendAxis);
+
+        const legendGradient = legendContainer
+          .append("defs")
+          .append("linearGradient")
+          .attr("id", "legend-gradient")
+          .attr("gradientTransform", "rotate(90)");
+
+        legendGradient
+          .append("stop")
+          .attr("offset", "0%")
+          .attr("stop-color", colorScale.range()[1]);
+
+        legendGradient
+          .append("stop")
+          .attr("offset", "100%")
+          .attr("stop-color", colorScale.range()[0]);
+
+        legendContainer
+          .append("rect")
+          .attr("x", 0)
+          .attr("y", 0)
+          .attr("width", legendWidth)
+          .attr("height", legendHeight)
+          .style("fill", "url(#legend-gradient)");
+      }
+
+      // Function to update the map based on the selected year and statistic
+      function updateMap(year, statistic = "deaths") {
+        const filteredData = data.filter((d) => d.year === year);
+        const regionsData = d3.rollup(
+          filteredData,
+          (v) => d3.mean(v, (d) => d[statistic]),
+          (d) => d.league
+        );
+        // Create a continuous color scale
+        const colorScale = d3
+          .scaleSequential(d3.interpolateBlues)
+          .domain(d3.extent(Array.from(regionsData.values())));
+
+        mapContainer
+          .selectAll(".tochange")
+          .style("opacity", 1)
+          .attr("fill", (d) => {
+            const region = idMapper(d.properties.name);
+            const value = regionsData.get(regionsMap[region]);
+            return value ? colorScale(value) : "grey";
+          });
+
+        updateLegend(colorScale);
+      }
+
+      // Initial map update
+      updateMap(d3.max(years));
+
+      // Update the legend when the slider value changes
+      slider.on("onchange", (year) => {
+        // Update the map based on the selected year
+        const selectedStat = document.getElementById("stat-dropdown").value;
+        updateMap(year, selectedStat);
+      });
+    });
   });
+}
+
+function part4() {
+  // Assuming you have an HTML element with id "chart" to render the chart
+  const svg = d3
+    .select("#plot-4-container")
+    .append("svg")
+    .attr("width", 800)
+    .attr("height", 400);
+
+  // Load the CSV data
+  d3.csv("champ_stats.csv")
+    .then((data) => {
+      // Get the available categories from the data
+      const categories = Object.keys(data[0]).slice(1);
+      console.log(categories);
+
+      // Create the dropdown menu
+      const dropdown = d3
+        .select("#dropdown")
+        .append("select")
+        .on("change", function () {
+          const selectedCategory = d3.select(this).property("value");
+          updateChart(selectedCategory);
+        });
+
+      dropdown
+        .selectAll("option")
+        .data(categories)
+        .enter()
+        .append("option")
+        .attr("value", (d) => d)
+        .text((d) => d);
+
+      // Function to update the chart based on the selected category
+      const updateChart = (category) => {
+        // Sort the data based on the selected category
+        data.sort((a, b) => b[category] - a[category]);
+        // Get the top 10 champions for the selected category
+        const topChampions = data.slice(0, 10);
+
+        // Clear the previous chart
+        svg.selectAll("*").remove();
+
+        // Define the chart dimensions
+        const margin = { top: 50, right: 20, bottom: 30, left: 40 };
+        const width = +svg.attr("width") - margin.left - margin.right;
+        const height = +svg.attr("height") - margin.top - margin.bottom;
+
+        // Create the x-axis scale
+        const xScale = d3
+          .scaleBand()
+          .rangeRound([0, width])
+          .padding(0.1)
+          .domain(topChampions.map((d) => d.champion));
+
+        // Create the y-axis scale
+        const yScale = d3
+          .scaleLinear()
+          .rangeRound([height, 0])
+          .domain([0, d3.max(topChampions, (d) => +d[category])]);
+
+        // Create the x-axis
+        svg
+          .append("g")
+          .attr(
+            "transform",
+            `translate(${margin.left}, ${height + margin.top})`
+          )
+          .call(d3.axisBottom(xScale));
+
+        // Create the y-axis
+        svg
+          .append("g")
+          .attr("transform", `translate(${margin.left}, ${margin.top})`)
+          .call(d3.axisLeft(yScale));
+
+        // Create the bars
+        svg
+          .selectAll(".bar")
+          .data(topChampions)
+          .enter()
+          .append("rect")
+          .attr("class", "bar")
+          .attr("x", (d) => xScale(d.champion) + margin.left)
+          .attr("y", (d) => yScale(+d[category]) + margin.top)
+          .attr("width", xScale.bandwidth())
+          .attr("height", (d) => height - yScale(+d[category]))
+          .attr("fill", "steelblue");
+
+        svg
+          .append("text")
+          .attr("x", width / 2 + margin.left)
+          .attr("y", margin.top / 2)
+          .attr("text-anchor", "middle")
+          .attr("font-weight", "bold")
+          .text(`Top 10 Champions - ${category}`);
+      };
+
+      // Initial chart
+      const initialCategory = categories[0];
+      updateChart(initialCategory);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 }
